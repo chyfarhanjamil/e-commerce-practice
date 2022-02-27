9bfbc8c7c828ad091d77f0697e4ee74373bc97a8
Chowdhury Farhan Jamil <56598076+chyfarhanjamil@users.noreply.github.com>
2022-02-27 11:18:17 +0600

    styled products

---
+++ e-commerce app design/src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "assets/dress1.jpg",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "assets/dress2.jpg",
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "assets/dress3.jpg",
+    title: "LOUNGEWEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "assets/shirt1.jpg",
+    title: "SHIRT STYLE!",
+  },
+  {
+    id: 2,
+    img: "assets/shirt2.jpg",
+    title: "LOUNGEWEAR LOVE",
+  },
+  {
+    id: 3,
+    img: "assets/shirt3.jpg",
+    title: "LIGHT JACKETS",
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: "assets/product1.jpg",
+  },
+  {
+    id: 2,
+    img: "assets/product2.jpg",
+  },
+  {
+    id: 3,
+    img: "assets/product3.jpg",
+  },
+  {
+    id: 4,
+    img: "assets/product4.jpg",
+  },
+  {
+    id: 5,
+    img: "assets/product5.jpg",
+  },
+  {
+    id: 6,
+    img: "assets/product6.jpg",
+  },
+  {
+    id: 7,
+    img: "assets/product7.jpg",
+  },
+  {
+    id: 8,
+    img: "assets/product8.jpg",
+  },
+];
