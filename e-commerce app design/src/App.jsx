3198b6476524f360ca437f6475816ebf7bd6082d
Chowdhury Farhan Jamil <56598076+chyfarhanjamil@users.noreply.github.com>
2022-02-27 17:30:20 +0600

    updated cart and product page for responsive design

--- e-commerce app design/src/App.jsx
+++ e-commerce app design/src/App.jsx
@@ -1,12 +1,12 @@
 //import Home from "./pages/Home";
-import Product from "./pages/Product";
+//import Product from "./pages/Product";
 //import ProductList from "./pages/ProductList";
 //import Login from "./pages/Login";
 // import Register from "./pages/Register";
-//import Cart from "./pages/Cart";
+import Cart from "./pages/Cart";
 
 const App = () => {
-  return <Product />;
+  return <Cart />;
 };
 
 export default App;
--- e-commerce app design/src/pages/Cart.jsx
+++ e-commerce app design/src/pages/Cart.jsx
@@ -5,11 +5,13 @@ import Navbar from "../components/Navbar";
 import Announcement from "../components/Announcement";
 import Footer from "../components/Footer";
 import { Add, Remove } from "@material-ui/icons";
+import { mobile } from "../responsive";
 
 const Container = styled.div``;
 
 const Wrapper = styled.div`
   padding: 20px;
+  ${mobile({ padding: "10px" })}
 `;
 
 const Title = styled.h1`
@@ -34,14 +36,19 @@ const TopButton = styled.button`
   color: ${(props) => props.type === "filled" && "white"};
 `;
 
-const TopTexts = styled.div``;
+const TopTexts = styled.div`
+  ${mobile({ display: "none" })}
+`;
+
 const TopText = styled.span`
   text-decoration: underline;
   cursor: pointer;
   margin: 0px 10px;
 `;
 
-const Bottom = styled.div``;
+const Bottom = styled.div`
+  ${mobile({ flexDirection: "column" })}
+`;
 
 const Info = styled.div`
   flex: 3;
@@ -50,6 +57,7 @@ const Info = styled.div`
 const Product = styled.div`
   display: flex;
   justify-content: space-between;
+  ${mobile({ flexDirection: "column" })}
 `;
 
 const ProductDetail = styled.div`
@@ -94,13 +102,17 @@ const ProductAmountContainer = styled.div`
   align-items: center;
   margin-bottom: 20px;
 `;
+
 const ProductAmount = styled.div`
   font-size: 24px;
   margin: 5px;
+  ${mobile({ margin: "5px 15px" })}
 `;
+
 const ProductPrice = styled.div`
   font-size: 30px;
   font-weight: 200;
+  ${mobile({ marginBottom: "20px" })}
 `;
 
 const Hr = styled.hr`
