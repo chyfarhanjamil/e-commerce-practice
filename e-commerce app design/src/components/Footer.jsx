6ee1ade096720a9bb742770964405bdd4781317f
Chowdhury Farhan Jamil <56598076+chyfarhanjamil@users.noreply.github.com>
2022-02-27 12:35:04 +0600

    Footer.jsx complete

--- e-commerce app design/src/components/Footer.jsx
+++ e-commerce app design/src/components/Footer.jsx
@@ -1,4 +1,12 @@
-import { Facebook, Instagram, Pinterest, Twitter } from "@material-ui/icons";
+import {
+  Facebook,
+  Instagram,
+  MailOutline,
+  Phone,
+  Pinterest,
+  Room,
+  Twitter,
+} from "@material-ui/icons";
 import React from "react";
 import styled from "styled-components";
 
@@ -13,16 +21,59 @@ const Left = styled.div`
 `;
 
 const Logo = styled.h1``;
-const Desc = styled.p``;
-const SocialContainer = styled.div``;
-const SocialIcon = styled.div``;
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+const SocialContainer = styled.div`
+  display: flex;
+`;
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+`;
 
 const Center = styled.div`
   flex: 1;
+  padding: 20px;
+`;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
 `;
 
 const Right = styled.div`
   flex: 1;
+  padding: 20px;
+`;
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`;
+
+const Payment = styled.img`
+  width: 50%;
 `;
 
 const Footer = () => {
@@ -32,22 +83,46 @@ const Footer = () => {
         <Logo>CHOWDHURY.</Logo>
         <Desc>CHOWDHURY FARHAN JAMIL Description</Desc>
         <SocialContainer>
-          <SocialIcon>
+          <SocialIcon color="3B5999">
             <Facebook />
           </SocialIcon>
-          <SocialIcon>
+          <SocialIcon color="E4405F">
             <Instagram />
           </SocialIcon>
-          <SocialIcon>
+          <SocialIcon color="55ACEE">
             <Twitter />
           </SocialIcon>
-          <SocialIcon>
+          <SocialIcon color="E60023">
             <Pinterest />
           </SocialIcon>
         </SocialContainer>
       </Left>
-      <Center></Center>
-      <Right></Right>
+      <Center>
+        <Title>Useful Links</Title>
+        <ListItem>Home</ListItem>
+        <ListItem>Cart</ListItem>
+        <ListItem>Men Fashion</ListItem>
+        <ListItem>Women Fashion</ListItem>
+        <ListItem>Accessories</ListItem>
+        <ListItem>My Account</ListItem>
+        <ListItem>Order Tracking</ListItem>
+        <ListItem>Wishlist</ListItem>
+        <ListItem>Wishlist</ListItem>
+        <ListItem>Terms</ListItem>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <Room style={{ marginRight: "10px" }} /> Uttara, Dhaka
+        </ContactItem>
+        <ContactItem>
+          <Phone style={{ marginRight: "10px" }} /> +88 01787356966
+        </ContactItem>
+        <ContactItem>
+          <MailOutline style={{ marginRight: "10px" }} /> chy.farhan4@gmail.com
+        </ContactItem>
+        <Payment src="#" />
+      </Right>
     </Container>
   );
 };
